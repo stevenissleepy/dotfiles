551f7cf6de05b63a4fb7258e1596ccb207ade2b4
steven <stevenyjc@outlook.com>
2026-04-20 16:50:12 +0800

    fix: nvim-treesitter config in nvim v0.12

--- configs/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,12 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	main = "nvim-treesitter",
 	build = ":TSUpdate",
-
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "cpp", "python", "c" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-
-      -- rainbow ()
-      rainbow = {
-				enable = true,
-				extended_mode = true, -- 启用超过两个括号的情况
-				max_file_lines = nil, -- 不限制文件行数
-			},
-		})
-	end,
+	opts = {
+		ensure_installed = { "lua", "cpp", "python", "c" },
+		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
